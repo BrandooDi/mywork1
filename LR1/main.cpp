bf220694cebd491acee62d97240f39890488154f
BrandooDi <nikita1231w@gmail.com>
2022-05-14 14:54:46 +0300

    4

--- LR1/main.cpp
+++ LR1/main.cpp
@@ -7,6 +7,8 @@ int main()
     cout << "Hi!" << endl;
      cout << "beautiful weather today" << endl;
      cout << "How are you doing?" << endl;
+     cout << "I would like to spend time with you today" << endl;
+
     return 0;
 
 }
